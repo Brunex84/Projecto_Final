b99aaf9f686aef102cc1bb53d9986dbd9d9f9b33
Brunex84 <134009209+Brunex84@users.noreply.github.com>
2024-03-20 00:01:05 +0000

    Add files via upload

--- src/pages/Tempo.jsx
+++ src/pages/Tempo.jsx
@@ -1,39 +1,52 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function FetchData() {
 	const [record, setRecords] = useState([]);
+	const [city, setCity] = useState('Bolho'); 
 
 	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/users')
+		fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_pt&APPID=8d81f0e12efe061269e2c7b1b9c821d8`)
 			.then(res => res.json())
 			.then(data => setRecords(data))
 			.catch(err => console.log(err));
-	}, []);
+	}, [city]); 
 
 	return (
 		<>
 			<Header />
-			<main class='main' id='main'>
-				<section class='section about-hero' style={{ backgroundImage: "url('./media/bg-about.jpg')" }} id='hero'>
-					<h1 class='hero__title'>Time</h1>
+			<main className='main' id='main'>
+				<section className='section about-hero' style={{ backgroundImage: "url('./media/bg-about.jpg')" }} id='hero'>
+					<h1 className='hero__title'>Weather</h1>
 				</section>
 
-				<section class='section about container'>
-					<div class='about__data'>
-						<h3 class='about__data-title'>Time table</h3>
-						load data somewhere here
+				<section className='section about container'>
+					<div className='about__data'>
+						<h3 className='about__data-tempo'>Weather Table</h3>
+					</div>
+					<div className='form__input-container'>
+						<input
+							onKeyDown={e => {
+								if (e.key === 'Enter') setCity(e.target.value);
+							}}
+							id='city-input'
+							type='text'
+							placeholder='Enter your City'
+						/>
 					</div>
 
-					<div>
+					<div className='about__data-weather'>
 						<ul>
-							{record.map((list, index) => (
-								<li key={index}>
-									{list.id} | {list.name}{' '}
-								</li>
-							))}
+							<li key={record.id}>
+								{record.name} | {record.sys?.country} |
+								{record.weather?.map(wt => (
+									<span key={wt.id}>
+										{wt.main} | {wt.temp}
+									</span>
+								))}
+								{parseInt(record.main?.temp)}ºC
+							</li>
 						</ul>
 					</div>
 				</section>
